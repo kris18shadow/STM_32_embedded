a70e22e2f2d27445c4a0f27e21f66438b39a8667
kris18shadow <kris18shadow@gmail.com>
2017-05-31 17:56:38 +0300

    First commit

---
+++ obj/Debug/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc.d
@@ -0,0 +1,22 @@
+obj\debug\stm32f4xx_hal_driver\src\stm32f4xx_hal_adc.o: \
+ STM32F4xx_HAL_Driver\Src\stm32f4xx_hal_adc.c \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal.h inc/stm32f4xx.h \
+ inc/stm32f401xe.h cmsis/core_cm4.h cmsis/core_cmInstr.h \
+ cmsis/core_cmFunc.h cmsis/core_cm4_simd.h inc/system_stm32f4xx.h \
+ inc/stm32f4xx_hal_conf.h STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_def.h \
+ STM32F4xx_HAL_Driver\Inc/Legacy/stm32_hal_legacy.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc_ex.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio_ex.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma_ex.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_cortex.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_adc.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_adc_ex.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ex.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ramfunc.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr_ex.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_spi.h
---
+++ obj/Debug/src/main.d
@@ -0,0 +1,21 @@
+obj\debug\src\main.o: src\main.c inc/main.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal.h inc/stm32f4xx.h \
+ inc/stm32f401xe.h cmsis/core_cm4.h cmsis/core_cmInstr.h \
+ cmsis/core_cmFunc.h cmsis/core_cm4_simd.h inc/system_stm32f4xx.h \
+ inc/stm32f4xx_hal_conf.h STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_def.h \
+ STM32F4xx_HAL_Driver\Inc/Legacy/stm32_hal_legacy.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc_ex.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio_ex.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma_ex.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_cortex.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_adc.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_adc_ex.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ex.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ramfunc.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr_ex.h \
+ STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_spi.h BSP/stm32f4xx_nucleo.h
